6ca0ecb3fc78f062da3aacde9fea310eb0b569a4
Taisei2 <tnbox3588git@gmail.com>
2026-05-14 13:25:07 +0900

    feat: クラス実行用アクションを追加

--- src/main/java/scoremanager/main/ClassCreateExecuteAction.java
+++ src/main/java/scoremanager/main/ClassCreateExecuteAction.java
@@ -1,14 +1,36 @@
 package scoremanager.main;
 
+import bean.ClassNum;
+import bean.Teacher;
+import dao.ClassNumDao;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
 public class ClassCreateExecuteAction extends Action {
 
   @Override
   public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+    // 予めクラス番号文字列を宣言
+    String classNumstr = "";
 
+    HttpSession session = req.getSession();
+    Teacher teacher = (Teacher) session.getAttribute("user");
+
+    // パラメータから値を取得
+    classNumstr = req.getParameter("num");
+
+    // 取得したパラメータでclassNumインスタンスを生成
+    ClassNum classNum = new ClassNum();
+    classNum.setClass_num(classNumstr);
+    classNum.setSchool(teacher.getSchool());
+
+    boolean result = new ClassNumDao().save(classNum);
+
+    if (result) {
+      req.getRequestDispatcher("class_create_done.jsp").forward(req, res);
+    }
 
   }
 
