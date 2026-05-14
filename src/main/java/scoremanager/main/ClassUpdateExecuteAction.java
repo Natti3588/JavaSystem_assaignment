34d438a3ef8b072b063533f42d33fa6301348213
test <te2195094@gmail.com>
2026-05-14 14:38:08 +0900

    修正

---
+++ src/main/java/scoremanager/main/ClassUpdateExecuteAction.java
@@ -0,0 +1,42 @@
+package scoremanager.main;
+
+import bean.ClassNum;
+import bean.Teacher;
+import dao.ClassNumDao;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class ClassUpdateExecuteAction extends Action{
+	  @Override
+	  public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+		  
+		HttpSession session = req.getSession(); // セッション
+		Teacher teacher = (Teacher) session.getAttribute("user");
+		// 1. パラメータの取得
+	    String oldclass_num = req.getParameter("old"); // 変更前のクラス番号
+	    String newclass_num = req.getParameter("num"); // 変更後のクラス番号
+
+	    // 2. ClassNumインスタンスの作成と値のセット
+	    ClassNum classnum = new ClassNum();
+	 // 前のクラスをインスタンスにセット
+	    classnum.setClass_num(oldclass_num); 
+	 // Schoolをセット
+	    classnum.setSchool(teacher.getSchool()); 
+	    
+
+	 // 3. 保存処理
+	    ClassNumDao classnumDao = new ClassNumDao();
+	    
+	    // classnum（Schoolセット済み）を第1引数に渡す
+	    classnumDao.save(classnum, newclass_num);
+
+	    // student_update_done.jspにフォワード
+	    req.getRequestDispatcher("class_update_done.jsp").forward(req, res);
+	  }
+
+	}
+
+
