10899ca95a0d8970fcd16e214363fbce7f9441c1
Taisei2 <tnbox3588git@gmail.com>
2026-05-19 09:51:49 +0900

    fix: 不要なセッションの取得の削除と、Loginエラー時のエラー文の修正

--- src/main/java/scoremanager/main/ClassUpdateAction.java
+++ src/main/java/scoremanager/main/ClassUpdateAction.java
@@ -1,22 +1,17 @@
 package scoremanager.main;
 
-import bean.Teacher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
 public class ClassUpdateAction extends Action {
-  
+
   @Override
   public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-    
-	  // セッション情報をリクエストから取得
-	    HttpSession session = req.getSession();
-	    Teacher teacher = (Teacher) session.getAttribute("user");
-//	    変更したいクラス番号を取得
-	 String old =req.getParameter("num");
-	 req.setAttribute("oldnum", old);
+
+    // 変更したいクラス番号を取得
+    String old = req.getParameter("num");
+    req.setAttribute("oldnum", old);
     // class_update.jspにフォワード
     req.getRequestDispatcher("class_update.jsp").forward(req, res);
   }
