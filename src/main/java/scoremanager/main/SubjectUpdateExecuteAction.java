0bf11bc0da8dff2a30db6a6187f1fcb2faa2c7c4
Taisei2 <tnbox3588git@gmail.com>
2026-05-20 14:43:29 +0900

    fix: 別ページからの削除に対応するための修正

--- src/main/java/scoremanager/main/SubjectUpdateExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectUpdateExecuteAction.java
@@ -10,29 +10,35 @@ import tool.Action;
 
 public class SubjectUpdateExecuteAction extends Action {
 
-	  @Override
-	  public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+  @Override
+  public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+    //// Subjectインスタンスを宣言
+    Subject subject = new Subject();
+    HttpSession session = req.getSession();
+    Teacher teacher = (Teacher) session.getAttribute("user");
+    //
+    //// 更新のための値をセット
+    subject.setCd(req.getParameter("cd"));
+    subject.setName(req.getParameter("name"));
+    subject.setSchool(teacher.getSchool());
+    //
+    //// subjectDaoインスタンスを宣言
+    SubjectDao subjectDao = new SubjectDao();
+    //
+    //// Daoでセットした値を保存
+    Subject isNull = subjectDao.get(subject.getCd(), subject.getSchool());
 
-////		  Subjectインスタンスを宣言
-		  Subject subject = new Subject();
-		  HttpSession session = req.getSession();
-		    Teacher teacher = (Teacher) session.getAttribute("user");
-//		  
-//		  
-////		  更新のための値をセット
-		  subject.setCd(req.getParameter("cd"));
-		  subject.setName(req.getParameter("name"));
-		  subject.setSchool(teacher.getSchool());
-//		  
-////		  subjectDaoインスタンスを宣言
-		  SubjectDao subjectDao = new SubjectDao();
-//		  
-////		  Daoでセットした値を保存
-		  subjectDao.save(subject);
-		  
-		   // subject_update_done.jspにフォワード
-		   req.getRequestDispatcher("subject_update_done.jsp").forward(req, res);
 
-		  
-	  }
+    if (isNull == null) {
+      String error = "科目が存在しません";
+      req.setAttribute("cd", subject.getCd());
+      req.setAttribute("error", error);
+      req.getRequestDispatcher("subject_update.jsp").forward(req, res);
+    } else {
+      subjectDao.save(subject);
+      // subject_update_done.jspにフォワード
+      req.getRequestDispatcher("subject_update_done.jsp").forward(req, res);
+    }
+
+  }
 }
